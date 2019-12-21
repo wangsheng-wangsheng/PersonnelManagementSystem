1b4f389e99131afa2f8c0306ba04f3389aba4a09
wang-jie99 <19861805218@163.com>
2019-12-22 00:44:11 +0800

    StaffService

--- PersonnelManagementSystem/src/service/StaffService.java
+++ PersonnelManagementSystem/src/service/StaffService.java
@@ -1,12 +1,34 @@
 package service;
+import dao.StaffDao;
 import domain.Staff;
 import java.sql.SQLException;
+import java.util.Collection;
+
 public class StaffService {
+    private static StaffDao staffDao = StaffDao.getInstance();
     private static StaffService staffService=new StaffService();
-    public static StaffService getInstance(){
+
+    public static StaffService getInstance() {
         return staffService;
     }
-    public Staff find(Integer id)throws SQLException {
-        return staffService.find(id);
+
+    public Collection<Staff> findAll() throws SQLException {
+        return staffDao.findAll();
+    }
+
+    public Staff find(Integer id) throws SQLException {
+        return staffDao.find(id);
+    }
+
+    public boolean update(Staff staff) throws SQLException {
+        return staffDao.update(staff);
+    }
+
+    public boolean add(Staff staff) throws SQLException {
+        return staffDao.add(staff);
+    }
+
+    public boolean delete(Integer id) throws SQLException {
+        return staffDao.getInstance().delete(id);
     }
 }
