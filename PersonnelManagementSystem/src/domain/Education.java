3b81e945dd6574ecb5c4a23d20d58edf087ba838
dell <1174552318@qq.com>
2019-12-22 17:03:56 +0800

    补充Education

--- PersonnelManagementSystem/src/domain/Education.java
+++ PersonnelManagementSystem/src/domain/Education.java
@@ -34,6 +34,19 @@ public class Education
 	public Education(){
 		super();
 	}
+	public Education(int id, String name){
+		this.id = id;
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+}
+	public String getName(){
+		return name;
+	}
+	public void setName(){
+		this.name = name;
+	}
 
 }
 
